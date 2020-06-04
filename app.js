c56d474b03c602f220bb74cb6e433f04051cabf8
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-04 00:37:18 -0700

    css and javascript restructuring

--- app.js
+++ app.js
@@ -1,202 +1,120 @@
-/* eslint-disable strict */
-/**
- * Example store structure
- */
-const STORE = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'Who directed the Titanic?',
-      answers: [
-        'James Cameron',
-        'Steven Spielberg',
-        'Stanley Kubrick',
-        'Michael Bay'
-      ],
-      correctAnswer: 'James Cameron'
-    },
-    {
-      question: 'Who plays Obediah Stane in Iron Man',
-      answers: [
-        'Jeff Bridges',
-        'Jeff Goldblum',
-        'Jeff Daniels',
-        'Robert Downey Jr.'
-      ],
-      correctAnswer: 'Jeff Bridges'
-    },
-    {
-      question: 'Which NBA player is NOT in Space Jam',
-      answer: [
-        'Scotty Pippen',
-        'Charles Pippen',
-        'Muggsy Bogues',
-        'Michael Jordan'
-      ],
-      correctAnswer: 'Scotty Pippen'
-    },
-    {
-      question: 'What was the tagline for Training Day',
-      answers: [
-        'King Kong ain’t got nothin’ on me!',
-        'Life is in their hands -- Death is on their minds.',
-        'On every street in every city in this country, there is a nobody who dreams of being a somebody.',
-        'On every street in every city in this country, there is a nobody who dreams of being a somebody.',
-        'The Happiest Sound in All the World'
-      ],
-      correctAnswer: 'King Kong ain’t got nothin’ on me!'
-    },
-    {
-      question: 'What was the name of the computer in 2001: A Space Odyssey',
-      answers: [
-        'Hal 9000',
-        'Hal 8000',
-        'Hal 6000',
-        'Commodore 64'
-      ],
-      correctAnswer: 'Hal 9000'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
-};
+'use strict';
+let questionsAndAnswers = [
+  {
+    question: 'Who directed the Titanic?',
+    answer1: 'James Cameron',
+    answer2: 'Stanley Kubrick',
+    answer3: 'Stanley Kubrick',
+    answer4: 'Michael Bay',
+    correctAnswer: 'James Cameron'
+  },
+  {
+    question: 'Who plays Obediah Stane in Iron Man?',
+    answer1: 'Jeff Bridges',
+    answer2: 'Jeff Goldblum',
+    answer3: 'Jeff Daniels',
+    answer4: 'Robert Downey Jr.',
+    correctAnswer: 'Jeff Bridges'
+  },
+  {
+    question: 'Which NBA player is NOT in Space Jam?',
+    answer1: 'Scotty Pippen',
+    answer2: 'Charles Barkley',
+    answer3: 'Mugsy Bogues',
+    answer4: 'Michael Jordan',
+    correctAnswer: 'Scotty Pippen'
+  },
+  {
+    question: 'What was the tagline for Training Day?',
+    answer1: 'King Kong ain’t got nothin’ on me!',
+    answer2: 'Life is in their hands -- Death is on their minds.',
+    answer3: 'On every street in every city in this country, there is a nobody who dreams of being a somebody.',
+    answer4: 'On every street in every city in this country, there is a nobody who dreams of being a somebody.',
+    correctAnswer: 'King Kong ain’t got nothin’ on me!'
+  },
+  {
+    question: 'What was the name of the computer in 2001: A Space Odyssey',
+    answer1: 'Hal 9000',
+    answer2: 'Hal 8000',
+    answer3: 'Hal 6000',
+    answer4: 'Commodore 64',
+    correctAnswer: 'Hal 9000'
+  },
+  {
+    quizStarted: false,
+    questionNumber: 0,
+    score: 0
+  }
+];
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
-
-/********** TEMPLATE GENERATION FUNCTIONS **********/
-
-// These functions return HTML templates
-
-//a function that returns the question template
-function questionTemplate() {
-  console.log('questionTemplate ran succesfully!');
-}
-
-//a function that returns the individual answer templates
-function answerTemplate() {
-  console.log('answerTemplate ran succesfully!');
-}
-
-//a function that creates the answers form 
-function answersFormTemplate() {
-  console.log('answersFormTemplate ran successfully!');
-}
-
-//a function display how far through the list of questions the user is (x out of 5 questions)
-function progressTemplate() {
-  console.log('progressTemplate ran successfully!');
-}
-
-//a function to return the final results template
-function finalResultsTemplate() {
-  console.log('finalResultsTemplate ran succesfully!');
-}
-
-/********** RENDER FUNCTION(S) **********/
-
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-// a function that renders the welcome screen
-function renderWelcomeScreen() {
-  console.log('renderWelcomeScreen ran succesfully!');
-  $('main').append(`<main>
-    <section>
-          <button type="submit" class= "glow-on-hover" id="start">Start</button>
-    </section>
-  </main>`);
-}
-
-// a function that renders the question screens
-function renderQuestionScreens() {
-  console.log('renderQuestions ran succesfully!');
-  $('main').append(`<main>
-    <section>
-      <div class= "image-box">
-        <img src="img/titanic.jpg" alt="ALT" width="WIDTH" height="HIEGHT">
-        </div>
-      <div class= "question-box">  
-        <h2>Question X of 5</h2>
-        <P>This is a question?</P>
-        
-        <form>
-          <div class= "input-selection">
-            <input type="radio" id="answer" name="gender" value="answer">
-            <label for="answers">${STORE.answer} </label>
-            </div>
-          <div class= "input-selection">
-            <input type="radio" id="answer" name="gender" value="answer">
-            <label for="answers">Answer2 </label>
-            </div>
-          <div class= "input-selection">
-            <input type="radio" id="answer" name="gender" value="answer">
-            <label for="answers">Answer3 </label>
-            </div>
-          <div class= "input-selection">
-            <input type="radio" id="answer" name="gender" value="answer">
-            <label for="answers">Answer4 </label>
-            </div>
-            <button type="submit" class= "glow-on-hover" id="submitbtn">Submit</button>   
-        </form>
-      </div>
-    </section>
-  </main>`);
-}
-
-// a function that renders the correct answer screen
-function renderCorrectScreen() {
-  console.log('rendomerCorrectScreen ran succesfully!');
-}
-
-//a function that renders the wrong answer screen
-function renderWrongScreen() {
-  console.log('renderWrongScreen ran successfully!');
-}
-
-// a function that renders the final results screen
-function renderFinalScreen() {
-  console.log('renderFinalScreen ran succesfully!');
-}
 
 /********** EVENT HANDLER FUNCTIONS **********/
-//Start Quiz button handler should call questions page render
+// Start Quiz button handler should call questions page render
 function handlerStart(){
 
-$("#start").submit(event => {
+  $('#start').click(event => {
     event.preventDefault();
-console.log(`this handler is recieving`)
-  //  return renderQuestionScreens();
-});
+    console.log('anything');
+    return renderQuestionScreens();
+  });
 }
-// These functions handle events (submit, click, etc)
+
+
 $(handlerStart);
 
-// $(questionTemplate);
-// $(answerTemplate)
-// $(answersFormTemplate);
-// $(progressTemplate);
-// $(finalResultsTemplate);
-$(renderWelcomeScreen);
-// $(renderQuestionScreens);
-// $(renderCorrectScreen);
-// $(renderWrongScreen);
-// $(renderFinalScreen);
-// console.log(STORE["questions"].answers);
+function handlerSubmit(){
+
+  $('form').submit(event => {
+    event.preventDefault();
+    console.log('anything again');
+    //  return renderQuestionScreens();
+  });
+}
 
 
+$(handlerSubmit);
 
 
+function renderQuestionScreens() {
+  console.log('renderQuestions ran succesfully!');
+  $('main').html(`<main>
+   <section>
+     <div class= "image-box">
+       <img src="img/titanic.jpg" alt="ALT" width="WIDTH" height="HIEGHT">
+       </div>
+     <div class= "question-box">  
+       <h2>Question X of 5</h2>
+       <P>This is a question?</P>
+       
+       <form>
+         <div class= "input-selection">
+           <input type="radio" id="answer" name="gender" value="answer">
+           <label for="answers">Answer1</label>
+           </div>
+         <div class= "input-selection">
+           <input type="radio" id="answer" name="gender" value="answer">
+           <label for="answers">Answer2 </label>
+           </div>
+         <div class= "input-selection">
+           <input type="radio" id="answer" name="gender" value="answer">
+           <label for="answers">Answer3 </label>
+           </div>
+         <div class= "input-selection">
+           <input type="radio" id="answer" name="gender" value="answer">
+           <label for="answers">Answer4 </label>
+           </div>
+           <button type="submit" class= "glow-on-hover" id="submitbtn">Submit</button>   
+       </form>
+     </div>
+   </section>
+ </main>`);
+}
 
+function countClicks() {
+  let clickCount = 0;
+  $('#submitbtn').click(function(event) {
+    clickCount += 1;
+  });
+}
+
+$(countClicks);
